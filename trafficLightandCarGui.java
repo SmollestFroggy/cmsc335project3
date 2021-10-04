8abe9e3ed70bba7529f267aff564798a683de8f5
SmollestFroggy <91900231+SmollestFroggy@users.noreply.github.com>
2021-10-04 14:50:39 -0400

    Add files via upload

--- trafficLightandCarGui.java
+++ trafficLightandCarGui.java
@@ -1,15 +1,8 @@
 package CMSC335_project3;
 
 
-
-import javax.swing.GroupLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JSlider;
-import javax.swing.JTable;
+import java.awt.Dimension;
+import javax.swing.*;
 
 
 
@@ -32,7 +25,7 @@ public class trafficLightandCarGui extends JFrame
 	
 	
 	
-	Object [][] carStateData = { {"Car #1", 0 , 0, 0}, {"Car #2", 0 , 0 ,0}, {"Car #3", 0 , 0 ,0}};
+	Object [][] carStateData = { {"Car #1", 200 , 0, 0}, {"Car #2", 0 , 0 ,0}, {"Car #3", 0 , 0 ,0}};
 	
 	
 	String[] columnNameLabels = {"Car#" , "X-Position" , "Y-Position" , "Speed(in Kilometers per hour)"};
@@ -51,7 +44,7 @@ public class trafficLightandCarGui extends JFrame
 	private void display()
 	{
 		
-		setSize(700,500);
+		setSize(800,500);
 		setVisible(true);
 		
 		// No one likes a window not in the center this next line allows for the frame to be centered to screen
@@ -68,11 +61,26 @@ public class trafficLightandCarGui extends JFrame
 		JLabel userInstruction1 = new JLabel("Please Click on the Start Button to start the Simulator!~~~ :)");
 		
 		//labels the intersections 1, 2 and 3)
-		JLabel trafficLight1at1000 = new JLabel("Intersection 1 (1000 Meters): ");
-		JLabel trafficLight2at2000 = new JLabel("Intersection 2 (2000 Meters): ");
-		JLabel trafficLight3at3000 = new JLabel("Intersection 3 (3000 Meters): ");
+		JLabel trafficLight1at1000 = new JLabel("Intersection 1 (1000 Meters):");
+		JLabel trafficLight2at2000 = new JLabel("Intersection 2 (2000 Meters):");
+		JLabel trafficLight3at3000 = new JLabel("Intersection 3 (3000 Meters):");
+		
+		car_1_Slider.setValue(0);
+		car_2_Slider.setValue(0);
+		car_3_Slider.setValue(0);
+	 
+		car_1_Slider.setMajorTickSpacing(1000);
+		car_1_Slider.setPaintTicks(true);
+		
+		car_2_Slider.setMajorTickSpacing(1000);
+		car_2_Slider.setPaintTicks(true);
+		
+		carDataTable.setPreferredScrollableViewportSize(new Dimension(300, 45));
+		carDataTable.setFillsViewportHeight(true);
+		
 		
 		JPanel carDataPanel = new JPanel();
+		
 		
 		// This is for allowing a table to be display in a panel in the GUI
 		JScrollPane scrollpaneForcarDataTable = new JScrollPane(carDataTable);
@@ -84,26 +92,25 @@ public class trafficLightandCarGui extends JFrame
 		
 		layout.setAutoCreateGaps(true);
 		layout.setAutoCreateContainerGaps(true);
-		layout.setHorizontalGroup(layout.createSequentialGroup().addContainerGap(20,20)
+		layout.setHorizontalGroup(layout.createSequentialGroup()
 				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER)
-						.addComponent(greetingTitle).addComponent(userInstruction1))
-			
-				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER)
+						.addComponent(greetingTitle).addComponent(userInstruction1)
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
 						 .addGroup(layout.createSequentialGroup()    
 				                    .addComponent(start).addComponent(pause).addComponent(stop)))       
 				                    .addComponent(car_1_Slider).addComponent(car_2_Slider).addComponent(car_3_Slider) 			                  
 				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
 						.addGroup(layout.createSequentialGroup()
-								.addComponent(trafficLight1at1000).addComponent(trafficLight1at1000Text).addContainerGap(25,25)
-								.addComponent(trafficLight2at2000).addComponent(trafficLight2at2000Text).addContainerGap(25,25)								
+								.addComponent(trafficLight1at1000).addComponent(trafficLight1at1000Text).addContainerGap(20,20) 
+								.addComponent(trafficLight2at2000).addComponent(trafficLight2at2000Text).addContainerGap(20,20)								
 								.addComponent(trafficLight3at3000).addComponent(trafficLight3at3000Text))						
-								.addComponent(carDataPanel))
-				.addContainerGap(25,25)
+								.addComponent(carDataPanel)))
+				.addContainerGap(10,10)
 		);
 		
 		layout.setVerticalGroup(layout.createSequentialGroup()
 				.addGroup(layout.createSequentialGroup().addComponent(greetingTitle).addComponent(userInstruction1))
-					.addGap(30, 30, 30)
+					.addGap(10, 10, 10)
 					.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
 							.addComponent(start).addComponent(pause).addComponent(stop))
 					.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
@@ -113,13 +120,13 @@ public class trafficLightandCarGui extends JFrame
 							.addComponent(trafficLight2at2000).addComponent(trafficLight2at2000Text)
 							.addComponent(trafficLight3at3000).addComponent(trafficLight3at3000Text))
 					.addComponent(carDataPanel)
-					.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE).addGap(30, 30, 30)) 
-					.addGap(30, 30, 30)
+					.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE).addGap(20, 20, 20)) 
+					.addGap(20, 20, 20)
 							
 		);
 							
 		
-		
+		pack();
 		
 		
 		
